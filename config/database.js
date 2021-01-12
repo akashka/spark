183b78012c683f016d94238bec6b9f25aa196ca6
Akash Agarwal <30892454+akashka@users.noreply.github.com>
2021-01-13 00:18:54 +0530

    asd
    sadasd

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-		url: "mongodb+srv://akash:abcd1234@cluster-7bl0f9cp.txl42.mongodb.net/heroku_7bl0f9cp?retryWrites=true&w=majority"
+	url: "mongodb://akash:abcd1234@cluster-7bl0f9cp-shard-00-00.txl42.mongodb.net:27017,cluster-7bl0f9cp-shard-00-01.txl42.mongodb.net:27017,cluster-7bl0f9cp-shard-00-02.txl42.mongodb.net:27017/heroku_7bl0f9cp?ssl=true&replicaSet=atlas-eel26w-shard-0&authSource=admin&retryWrites=true&w=majority"
 //  	url: "mongodb://akash:abcd1234@ds163053.mlab.com:63053/heroku_7bl0f9cp"
 	// url: "mongodb://localhost:27017/spark"
 }
