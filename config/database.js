7914f804fb3120421232f40a17f1e2c916ec3086
Akash Agarwal <30892454+akashka@users.noreply.github.com>
2021-01-12 22:13:35 +0530

    db update
    db update

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-	url: "mongodb://heroku_7bl0f9cp:c765avlu1acm9pvbu1t56mnmfa@ds163053.mlab.com:63053/heroku_7bl0f9cp"
+	url: "mongodb+srv://heroku_7bl0f9cp:c765avlu1acm9pvbu1t56mnmfa@cluster-7bl0f9cp.txl42.mongodb.net/heroku_7bl0f9cp?retryWrites=true&w=majority"
+// 	url: "mongodb://heroku_7bl0f9cp:c765avlu1acm9pvbu1t56mnmfa@ds163053.mlab.com:63053/heroku_7bl0f9cp"
 	// url: "mongodb://localhost:27017/spark"
 }
